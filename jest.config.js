1f95ca665c67c7d19a58044eb5d1b31523a2d5f6
qwerty0121 <sh1ng0718o@gmail.com>
2023-09-18 17:45:07 +0900

    [add] jest-environment-jsdomの導入

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons']
+  },
   transform: {
     '^.+\\.js$': 'babel-jest',
     '^.+\\.vue$': '@vue/vue3-jest'
